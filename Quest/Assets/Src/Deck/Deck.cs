471e31c48171c4334ccccc37e7a66ab0f4b79907
Roy Xia <xia_roy@hotmail.com>
2018-01-30 09:04:04 -0500

    Deck class and progress report iteration 2

---
+++ Quest/Assets/Src/Deck/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Deck : Card { 
+
+    public Deck(string name, CardTypes type) : base (name, type) 
+    {
+
+    }
+
+    public Card Draw () //draw a card //unsure
+    {
+
+    }
+
+    public void Add(Card card) //adds a card void
+    {
+        
+    }
+
+    public void Shuffle() //shuffles deck void
+    {
+
+    }
+
+
+
+
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    //void Update () {
+
+    //}
+}
