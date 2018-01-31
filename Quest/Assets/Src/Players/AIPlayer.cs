1095fb2595ba3f4d1ec08d046e2ffd6c1157be6d
qudus <qudus@DESKTOP-MAVPHM7>
2018-01-31 13:14:41 -0500

    updated AI player class

--- Quest/Assets/Src/Players/AIPlayer.cs
+++ Quest/Assets/Src/Players/AIPlayer.cs
@@ -5,16 +5,12 @@ public class AIPlayer : Player
     public int i;
     public string str;
 
-	public AIPlayer()
+	public AIPlayer(string name):base(name)
 	{
-        super();
 	}
 
-    public boolean playCard(Hand hand)
+    public override bool playCard(Hand hand)
     {
-        /*will randomly select cards for which AI will play
-         * */
-        return null;
+        throw new NotImplementedException();
     }
-
 }
