07954cbcc5e14fe22ce06548c202320b56c2849a
Jacob Martin <jacob@martinkids.ca>
2018-01-31 13:15:12 -0500

    Fixed Compile errors in Ai Player Added enumerator for selecting weapon type

--- Quest/Assets/Src/Cards/WeaponCard.cs
+++ Quest/Assets/Src/Cards/WeaponCard.cs
@@ -4,9 +4,59 @@ using System.Collections;
 public abstract class WeaponCard : AdventureCard
 {
 
-    public WeaponCard(string name, int battlePoints) : base(name, CardTypes.Weapon, battlePoints) { }
+    public WeaponCard(WeaponTypes type) : base(getWeaponName(type), CardTypes.Weapon, getBattlePoints(type)) { }
 
     public override bool hasEffect() { return false; }
 
     public override void doEffect(/*GameState gState*/) { }
+
+    private static string getWeaponName(WeaponTypes type)
+    {
+        switch (type)
+        {
+            case WeaponTypes.Dagger:
+                return "Dagger";
+            case WeaponTypes.Sword:
+                return "Sword";
+            case WeaponTypes.Horse:
+                return "Horse";
+            case WeaponTypes.BattleAx:
+                return "Battle Axe";
+            case WeaponTypes.Lance:
+                return "Lance";
+            case WeaponTypes.Excalibur:
+                return "Excalibur";
+        }
+        return null;
+    }
+
+    private static int getBattlePoints(WeaponTypes type)
+    {
+        switch (type)
+        {
+            case WeaponTypes.Dagger:
+                return 5;
+            case WeaponTypes.Sword:
+                return 10;
+            case WeaponTypes.Horse:
+                return 10;
+            case WeaponTypes.BattleAx:
+                return 15;
+            case WeaponTypes.Lance:
+                return 20;
+            case WeaponTypes.Excalibur:
+                return 30;
+        }
+        return 0;
+    }
+}
+
+public enum WeaponTypes
+{
+    Dagger,
+    Sword,
+    Horse,
+    BattleAx,
+    Lance,
+    Excalibur,
 }
--- Quest/Assets/Src/Players/AIPlayer.cs
+++ Quest/Assets/Src/Players/AIPlayer.cs
@@ -5,12 +5,12 @@ public class AIPlayer : Player
     public int i;
     public string str;
 
-	public AIPlayer()
+	public AIPlayer() : base("AI")
 	{
-        super();
+        
 	}
 
-    public boolean playCard(Hand hand)
+    public override bool playCard(Hand hand)
     {
         /*will randomly select cards for which AI will play
          * */
