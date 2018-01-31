0438d63133d9b8b9ba10d5fdb68257645b1d33cf
qudus <qudus@DESKTOP-MAVPHM7>
2018-01-31 12:50:26 -0500

    Created Player Class

---
+++ Quest/Assets/Src/Players/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class Player {
+
+    private string name;
+    private int rank, shields;
+    private Hand hand;
+    private Deck playPile;
+
+    /*creating a new Player
+     * input : name (string) - name of player
+     * */
+    public Player(string name)
+    {
+        this.name = name;
+        this.rank = 1;
+        this.shields = 0;
+    }
+    /*constructor to create a player from given attributes
+     * would be good for test cases
+     * */
+    public Player(string name, int rank, int shields, Hand hand)
+    {
+        name = name;
+        rank = rank;
+        shields = shields;
+        hand = hand;
+    }
+    //getters
+    public string getName() { return name; }
+
+    public int getRank() { return rank; }
+
+    //setters
+    public void setName(String name) { this.name = name; }
+
+    public void increaseRank() { rank++; }
+
+    public int currNumShields() { return shields; }
+
+
+    public bool addCard(Card card) {
+        if (hand.addCard(card) == true){
+            Console.WriteLine(" Card added successfullyy");
+            return true;
+        }else
+        {
+            Console.WriteLine(" Cannot add card to Hand");
+            return false;
+        }
+    }
+    public abstract bool playCard(Hand hand);
+}
---
+++ Quest/Assets/Src/Players/UserPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class UserPlayer : Player
+{
+    public UserPlayer(string name):base(name)
+	{
+        
+	}
+
+    public override bool playCard(Hand hand)
+    {
+        throw new NotImplementedException();
+    }
+}
