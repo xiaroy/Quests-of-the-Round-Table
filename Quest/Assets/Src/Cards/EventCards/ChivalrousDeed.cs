f16057f807c9cbff8d5dc29e7e81bd3843bb4f4a
Roy Xia <xia_roy@hotmail.com>
2018-02-22 13:00:44 -0500

    Chivalrous Deed Event

---
+++ Quest/Assets/Src/Cards/EventCards/ChivalrousDeed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChivalrousDeed : EventCard
+{
+    public ChivalrousDeed() : base("Chivalrous Deed") { }
+
+    public override void doEffect(GameState gState)
+    {
+        Player[] players = gState.getPlayersInRankOrder();
+        Player[] lowest = gState.getPlayers();
+        int i = 1;
+        lowest[0] = players[0];
+
+        foreach (Player player in gState.getPlayers())
+        {
+            if(player.GetRank() == players[players.Length - 1].GetRank())
+            {
+                lowest[i] = players[players.Length - 1];
+                i = i + 1;
+            }
+        }
+
+        foreach (Player p in lowest)
+        {
+            p.addShields(3);
+        }
+    }
+}
