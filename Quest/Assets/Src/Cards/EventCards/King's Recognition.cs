0bfcda8bfa074e7d659a21ec384e784451aab079
Roy Xia <xia_roy@hotmail.com>
2018-02-22 12:06:28 -0500

    King's Recogintion Card

---
+++ Quest/Assets/Src/Cards/EventCards/King's Recognition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingsRecognition : EventCard
+{
+
+    public KingsRecognition() : base("King's Recognition") { }
+
+    public override void doEffect(GameState gState)
+    {
+        //The next player(s) that complete a quest get 2 extra shields
+    }
+}
